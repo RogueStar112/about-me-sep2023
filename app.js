2237eb87799cbc5f04e148d4701c011f78ab0241
RogueStar112 <hildtgunnt@gmail.com>
2023-09-19 11:48:16 +0100

    Added quiz

---
+++ app.js
@@ -0,0 +1,45 @@
+let q1 = prompt("Demie has worked before in archaeology. Yes/No?");
+
+if (q1.toLowerCase() == "n" || q1.toLowerCase == "no") {
+  console.log("Correct! I've not worked in Archaeology before.");
+} else {
+  console.log("Incorrect! I've not worked in Archaeology.");
+}
+
+let q2 = prompt("Demie is double-jointed. Yes/No?");
+
+if (q2.toLowerCase() == "y" || q2.toLowerCase == "yes") {
+  console.log("Correct! I'm double jointed in my fingers!");
+} else {
+  console.log("Incorrect! I'm double jointed in my fingers!");
+}
+
+let q3 = prompt("I like EDM music. Yes/No?");
+
+if (q3.toLowerCase() == "y" || q3.toLowerCase == "yes") {
+  console.log(
+    "Correct! Alan Walker, Avicii, and Gryffin are my fav EDM artists!"
+  );
+} else {
+  console.log("Incorrect! I DO like EDM music!");
+}
+
+let q4 = prompt("I unironically use Red theme on my VSCode. Yes/No?");
+
+if (q4.toLowerCase() == "y" || q4.toLowerCase == "yes") {
+  console.log(
+    "Correct! Red theme gives me very cool vibes. (Even though it's warm)"
+  );
+} else {
+  console.log("Incorrect! I use red theme. Muhahaha.");
+}
+
+let q5 = prompt("My favourite dish is Thai Green Curry. Yes/No?");
+
+if (q5.toLowerCase() == "y" || q5.toLowerCase == "yes") {
+  console.log(
+    "Correct! Thai Green Curry is spicy, sweet, and savoury, all in one!"
+  );
+} else {
+  console.log("Incorrect! I love Thai green curry!");
+}
