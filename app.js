19669807b1845c4ba60e439b252ea252b07dc5ef
RogueStar112 <hildtgunnt@gmail.com>
2023-09-19 20:28:08 +0100

    Added Photo Carousel

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let q_name = prompt("What is your name?");
 
 let q1 = prompt("Demie has worked before in archaeology. Yes/No?");
 
-if (q1.toLowerCase() == "n" || q1.toLowerCase == "no") {
+if (q1.toLowerCase() == "n" || q1.toLowerCase() == "no") {
   // console.log("Correct! I've not worked in Archaeology before.");
   alert("Correct! I've not worked in Archaeology before.");
   points += 1;
@@ -15,7 +15,7 @@ if (q1.toLowerCase() == "n" || q1.toLowerCase == "no") {
 
 let q2 = prompt("Demie is double-jointed. Yes/No?");
 
-if (q2.toLowerCase() == "y" || q2.toLowerCase == "yes") {
+if (q2.toLowerCase() == "y" || q2.toLowerCase() == "yes") {
   // console.log("Correct! I'm double jointed in my fingers!");
   alert("Correct! I'm double jointed in my fingers!");
   points += 1;
@@ -26,7 +26,7 @@ if (q2.toLowerCase() == "y" || q2.toLowerCase == "yes") {
 
 let q3 = prompt("I like EDM music. Yes/No?");
 
-if (q3.toLowerCase() == "y" || q3.toLowerCase == "yes") {
+if (q3.toLowerCase() == "y" || q3.toLowerCase() == "yes") {
   // console.log(
   //   "Correct! Alan Walker, Avicii, and Gryffin are my fav EDM artists!"
   // );
@@ -39,7 +39,7 @@ if (q3.toLowerCase() == "y" || q3.toLowerCase == "yes") {
 
 let q4 = prompt("I unironically use Red theme on my VSCode. Yes/No?");
 
-if (q4.toLowerCase() == "y" || q4.toLowerCase == "yes") {
+if (q4.toLowerCase() == "y" || q4.toLowerCase() == "yes") {
   // console.log(
   //   "Correct! Red theme gives me very cool vibes. (Even though it's warm)"
   // );
@@ -52,7 +52,7 @@ if (q4.toLowerCase() == "y" || q4.toLowerCase == "yes") {
 
 let q5 = prompt("My favourite dish is Thai Green Curry. Yes/No?");
 
-if (q5.toLowerCase() == "y" || q5.toLowerCase == "yes") {
+if (q5.toLowerCase() == "y" || q5.toLowerCase() == "yes") {
   // console.log(
   //   "Correct! Thai Green Curry is spicy, sweet, and savoury, all in one!"
   // );
--- photo_roll.js
+++ photo_roll.js
@@ -4,6 +4,22 @@ let photoList = ["img_1", "img_2", "img_3", "img_4", "img_5"];
 
 let unselectedPhotos = document.querySelectorAll(".unselected");
 
+let photoRoll = document.getElementById("photo-roll");
+
+let photoPosition = 0;
+
+function getPhotoRollWidth() {
+  let photoRollTotalLength = 0;
+
+  for (i = 0; i < photoList.length; i++) {
+    let photoSelected = document.getElementById(photoList[i]);
+    photoRollTotalLength += photoSelected.offsetWidth;
+  }
+
+  console.log("TOTAL PHOTO ROLL WIDTH " + photoRollTotalLength);
+  return photoRollTotalLength;
+}
+
 function prevPhoto() {
   photoIndex -= 1;
 
@@ -11,25 +27,40 @@ function prevPhoto() {
     /* get photoIndex length, minus 1, to account for indexing
      starting with 0 */
     photoIndex = photoList.length - 1;
+    photoPosition = getPhotoRollWidth();
+    photoRoll.style.left = `${photoPosition}` + "px";
+  } else {
+    console.log(photoList[photoIndex]);
+
+    let photoSelected = document.getElementById(photoList[photoIndex]);
+
+    photoSelected.classList.toggle("selected");
+
+    photoPosition -= photoSelected.offsetWidth;
+    photoRoll.style.left = `-${photoPosition}` + "px";
+
+    console.log(`PHOTOSELECTED OFFSETWIDTH: ${photoPosition}`);
   }
-
-  console.log(photoList[photoIndex]);
-
-  let photoSelected = document.getElementById(photoList[photoIndex]);
-
-  photoSelected.classList.toggle("selected");
 }
 
 function nextPhoto() {
   photoIndex += 1;
 
   if (photoIndex > photoList.length - 1) {
+    console.log("Photo limit exceeded");
     photoIndex = 0;
+    photoPosition = 0;
+    photoRoll.style.left = "0px";
+  } else {
+    let photoSelected = document.getElementById(photoList[photoIndex]);
+
+    photoSelected.classList.toggle("selected");
+
+    photoPosition += photoSelected.offsetWidth;
+    photoRoll.style.left = `-${photoPosition}` + "px";
+
+    console.log(`PHOTOSELECTED OFFSETWIDTH: ${photoPosition}`);
   }
 
-  console.log(photoList[photoIndex]);
-
-  let photoSelected = document.getElementById(photoList[photoIndex]);
-
-  photoSelected.classList.toggle("selected");
+  // console.log(photoList[photoIndex]);
 }
